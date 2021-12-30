f6b632cb1e900ac539c567f97e04b70be8b464da
arturodcv <59840729+arturodcv@users.noreply.github.com>
2021-12-30 12:53:52 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -43,8 +43,8 @@ num_orientations = 4
 
 #Layers
 extent = [float(num_hipercolumns), float(num_hipercolumns)]
-ratio_exc_inh = 1
-neurons_per_column_inh = 1
+ratio_exc_inh = 4
+neurons_per_column_inh = 5
 neurons_per_column_exc = ratio_exc_inh * neurons_per_column_inh 
 poisson_bias = 0.0
 
