66ec442203800bc86ce58b1a598a0fc511c55a98
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-01-24 12:10:46 +0100

    Update results.py

--- results.py
+++ results.py
@@ -37,9 +37,10 @@ path = results_path + '/data_inh'; create_folder(path) ; remove_contents(path)
 data = read_and_fix_dataframe('','inh')
 times,complementary_time_list = get_times(data)
 eeg = get_eeg(times, complementary_time_list, 'inh', '_', path)
+print('inhibitory spikes: ',np.sum(eeg[200:]))
 freqs, peaks,values, idx = get_frequencies(eeg,'inh','_', path)
 inh_activity = np.sum(eeg)
-print('inhibitory spikes: ',np.sum(eeg[200:]))
+
 
 print("\n\nResults for excitatory data: ")
 
@@ -48,9 +49,10 @@ path = results_path + '/data_exc'; create_folder(path) ; remove_contents(path)
 data = read_and_fix_dataframe('','exc')
 times,complementary_time_list = get_times(data)
 eeg = get_eeg(times, complementary_time_list, 'exc', '_', path)
+print('excitatory spikes: ',np.sum(eeg[200:]))
 freqs, peaks, values, idx = get_frequencies(eeg,'exc','_', path)
 exc_activity = np.sum(eeg)
-print('excitatory spikes: ',np.sum(eeg[200:]))
+
 
 collect_data(eeg, image_selected, exc_activity, inh_activity, peaks, values, idx)
 
