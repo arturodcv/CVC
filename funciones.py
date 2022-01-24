eb44261ecfd8964329d159848b9df31d1c0c05eb
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-01-24 14:40:04 +0100

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -344,7 +344,7 @@ def get_frequencies(eeg,orientation_to_read,exc_or_inh, path):
     plt.close('all')
     return density, peaks
     
-def collect_data(image_selected, exc_eeg, inh_eeg, peaks, freqs, seed):
+def collect_data(image_selected, exc_eeg, inh_eeg, peaks, freqs, idx, seed):
     dictionary = {'image_name': image_selected, 'exc_activity': np.sum(exc_eeg), 'inh_activity': np.sum(inh_eeg), 
                   'exc_spikes_from': np.sum(exc_eeg[200:]), 'inh_spikes_from': np.sum(inh_eeg[200:]),
                   'node': peaks[idx][0], 'gamma_power': np.around(sum(freqs[broadband_initial:broadband_end_1]),2) ,
