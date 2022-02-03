1a175cba359d12793c16ec1a55ec5a1e6847d1b3
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-03 14:08:47 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -6,7 +6,7 @@ results_path = 'results_folder'
 positions_path = 'positions_folder'
 input_images_path = 'input_images_folder' 
 gabor_folder = 'gabor_outputs'
-collect_data_folder = 'results_collect_data_21'
+collect_data_folder = 'results_collect_data_22'
 
 
 #Simulation 
@@ -93,14 +93,14 @@ slowness_exc = 1.0; slowness_exc_large = 1.0; slowness_inh = 0.05
 # Large lateral connections
 kappa_j = 0.126 * 1
 kappa_w = 0.14 * 1
-weight_large_range_exc_exc = 0.1
+weight_large_range_exc_exc = 0.02
 weight_large_range_exc_inh = 0.1
 rescale = 2.0 ; radius_lat = 2.5
 
 # Short lateral connections
 ratio_inh_exc_w = 4.0
-p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.2
-p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.2
+p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.15
+p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.15
 p_center_exc_exc = 1.0 ;        weight_exc_exc =  0.5
 p_center_exc_inh = 1.0 ;        weight_exc_inh =  0.5
 
