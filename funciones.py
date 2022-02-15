3f6b523868b3d64a395dc3cb4dd9d194cbce5916
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-15 14:55:49 +0100

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -125,7 +125,7 @@ def get_image_with_frequencies(image_name,orientation_in_radians, max_freq):
     fp = csf(freqs)    
     normalized_fp = fp/np.sum(fp)
     fd = [np.sum(normalized_fp[:i]) for i in range(len(normalized_fp))] 
-    samples = get_numbers_from_distribution(fd,freqs,cortex_size)
+    samples = get_samples_from_distribution(fd,freqs,cortex_size)
     #samples = np.random.randint(1,max_freq+1,cortex_size)
     
     images = []
