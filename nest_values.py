eb56cfe49589ca43eb1f664eb785ba572e31ad22
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-03-02 09:33:19 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -173,7 +173,7 @@ image_from = int(ms_steady_state_simulation) + 100
 eeg_freqs_from = int(ms_steady_state_simulation) + 200
 eeg_freqs_until = eeg_freqs_from + 500
 radius = 2.5
-make_image_video = True
+make_image_video = False
 collect_data_bool = 0
 
 
